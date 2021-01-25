f2b6860ae91a6ae23d14d7f5e383b5da6a10a319
Moayed Haji Ali <mali18@ku.edu.tr>
2021-01-25 17:41:51 +0300

    add dbscan clustering

--- src/experiments/RegistrationMLP.py
+++ src/experiments/RegistrationMLP.py
@@ -16,11 +16,29 @@ from tools.StrokeClustering import DensityClustering
 from tools.ParsingEvaluation import ParsingEvaluation
 from registrationNN.models import registration_model
 import time
+import pathlib
+import os.path as path
+import matplotlib.pyplot as plt
 
 
 # train a MLP for registration
-FILE = '../input_directory/samples/test_samples/single_strokes_circles.xml'
+dir = 'ASIST_Dataset/Data/Data_B/Circle'
+dir = path.join(path.abspath(path.join(__file__ ,"../../..")), dir)
+objs, labels = [], []
+n, N = 0, 100
+for path in pathlib.Path(dir).iterdir():
+    print(path)
+    if n >= N:
+        break
+    a, b = ObjectUtil.xml_to_UnlabeledObjects(str(path))
+    objs.extend(a)
+    labels.extend(b)
+    n += len(a)
 
-objs, labels = ObjectUtil.xml_to_UnlabeledObjects(FILE, mn_len=5)
-print(labels)
+# print(len(objs))
+# fig, axs = plt.subplots(4, 4)
+# for i, obj in enumerate(objs):
+#     obj.visualize(ax=axs[int(i/4), int(i%4)], show=False)
+
+# plt.show()
 registration_model(objs)
